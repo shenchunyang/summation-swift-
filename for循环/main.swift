a8ea5f2041c80ca2ecc7809aa6664d524eb157cd
shenchunyang <703958897@qq.com>
2016-06-20 11:36:59 +0800

    累计相加
    在swift编程语言中，1到100累计相加，使用到的循环语句for a in
    b{};将b看作一个集合，a看作一个子集，判断a是否在集合b里面，从而执行循环体内的语句，得到累积相加的效果。

---
+++ for循环/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  for循环
+//
+//  Created by ry耷拉拉星球 on 16/6/20.
+//  Copyright © 2016年 ry耷拉拉星球. All rights reserved.
+//
+
+import Foundation
+
+var a = 1,sum=0,b=0...100;
+for a in b{
+    sum=sum+a
+
+}
+print(sum)
